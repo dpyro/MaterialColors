787108fc2f0d093a3bb6a166f9e0465bc9850022
Sumant Manne <sumant.manne@gmail.com>
2016-01-18 14:13:42 -0600

    Initial Commit

---
+++ MaterialColors/MaterialColors.h
@@ -0,0 +1,19 @@
+//
+//  MaterialColors.h
+//  MaterialColors
+//
+//  Created by Sumant Manne on 1/18/16.
+//  Copyright © 2016 Sumant Manne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialColors.
+FOUNDATION_EXPORT double MaterialColorsVersionNumber;
+
+//! Project version string for MaterialColors.
+FOUNDATION_EXPORT const unsigned char MaterialColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialColors/PublicHeader.h>
+
+
